7713cf42473e043d0255136894f0a3812d4bb6ee
Gustavo E Corrales <gespinozac@est.utn.ac.cr>
2023-12-04 14:59:39 -0600

    2fa google

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\Auth\TwoFactorController;
 
 // Home
 
@@ -19,3 +20,7 @@ Route::post('sessions',[SessionsController::class,'store'])->middleware('guest')
 // Register
 Route::get('register',[RegisterController::class,'create'])->middleware('guest');
 Route::post('register',[RegisterController::class,'store'])->middleware('guest');
+
+// 2FA
+// Route::get('/2fa', [TwoFactorController::class,'show'])->name('2fa');
+// Route::post('/2fa', [TwoFactorController::class,'verify'])->name('2fa.verify');
